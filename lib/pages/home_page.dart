43c95bb38e1cc820549337292897f8dda19c24fa
Erynyx <redvat3@gmail.com>
2025-12-02 15:13:32 +0700

    Add kategori makanan & minuman + update HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,128 +9,156 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Pisahkan kategori menu
+    final List<MenuItem> foodMenu =
+        menuData.where((item) => item.isDrink == false).toList();
+
+    final List<MenuItem> drinkMenu =
+        menuData.where((item) => item.isDrink == true).toList();
+
     return Scaffold(
-      backgroundColor: Colors.white, // warna background aplikasi
+      backgroundColor: Colors.white,
 
       appBar: AppBar(
         title: const Text(
           "Rizora Cafe's",
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: Colors.orange, // warna header
+        backgroundColor: Colors.orange,
         centerTitle: true,
-        elevation: 0, // tanpa shadow
+        elevation: 0,
       ),
 
       body: SingleChildScrollView(
-        // agar halaman bisa discroll
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // judul section "Daftar Menu"
-            const SectionTitle(title: "Daftar Menu"),
+
+            // KATEGORI MAKANAN
+            const SectionTitle(title: "Makanan"),
 
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
               child: GridView.builder(
-                itemCount: menuData.length, // jumlah item menu
-                shrinkWrap: true, // agar GridView menyesuaikan height
+                itemCount: foodMenu.length,
+                shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
-                // GridView tidak scroll sendiri
 
-                // pengaturan grid 2 kolom
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, // 2 item per baris
+                  crossAxisCount: 2,
                   crossAxisSpacing: 16,
                   mainAxisSpacing: 16,
                   childAspectRatio: 0.82,
-                  // rasio tinggi-lebar card
                 ),
 
                 itemBuilder: (context, index) {
-                  final MenuItem item = menuData[index]; // ambil item menu
+                  final MenuItem item = foodMenu[index];
 
-                  return InkWell(
-                    borderRadius: BorderRadius.circular(14),
-                    // animasi klik mengikuti radius card
-
-                    onTap: () {
-                      // pindah ke halaman detail menu
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => DetailPage(menu: item),
-                        ),
-                      );
-                    },
-
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(14),
-                        boxShadow: [
-                          BoxShadow(
-                            // bayangan lembut card
-                            color: Colors.black.withValues(alpha: 0.1),
-                            blurRadius: 8,
-                            offset: const Offset(2, 3),
-                          ),
-                        ],
-                      ),
-
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          // bagan foto menu
-                          ClipRRect(
-                            borderRadius: const BorderRadius.vertical(
-                              top: Radius.circular(14),
-                            ),
-                            child: Image.asset(
-                              item.image, // gambar menu
-                              height: 180, // diperbesar agar lebih penuh
-                              width: double.infinity,
-                              fit: BoxFit.cover,
-                              // gambar memenuhi area tanpa distorsi
-                            ),
-                          ),
-
-                          // nama menu
-                          Padding(
-                            padding: const EdgeInsets.all(10),
-                            child: Text(
-                              item.name,
-                              maxLines: 2, // maksimal 2 baris
-                              overflow: TextOverflow.ellipsis,
-                              // membuat titik (...) kalau nama terlalu panjang
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-
-                          // harga menu
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: Text(
-                              "Rp ${item.price}",
-                              style: const TextStyle(
-                                fontSize: 14,
-                                color: Colors.orange,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-
-                          const SizedBox(height: 8), // jarak bawah
-                        ],
-                      ),
-                    ),
-                  );
+                  return _buildMenuCard(context, item);
                 },
               ),
             ),
+
+            const SizedBox(height: 20),
+
+            //  KATEGORI MINUMAN
+            const SectionTitle(title: "Minuman"),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: GridView.builder(
+                itemCount: drinkMenu.length,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  childAspectRatio: 0.82,
+                ),
+
+                itemBuilder: (context, index) {
+                  final MenuItem item = drinkMenu[index];
+                  return _buildMenuCard(context, item);
+                },
+              ),
+            ),
+
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Card Menu reusable
+  Widget _buildMenuCard(BuildContext context, MenuItem item) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(14),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => DetailPage(menu: item),
+          ),
+        );
+      },
+
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(14),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.1),
+              blurRadius: 8,
+              offset: const Offset(2, 3),
+            ),
+          ],
+        ),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(14),
+              ),
+              child: Image.asset(
+                item.image,
+                height: 180,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                item.name,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                "Rp ${item.price}",
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Colors.orange,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 8),
           ],
         ),
       ),
